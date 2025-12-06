a66a8bc4a0406139a6677e6deeedbfbce06de569
The1Ankit <ankitankush166@gmail.com>
2025-12-06 07:03:04 +0000

    save

---
+++ 03_basics/01_function.js
@@ -0,0 +1,18 @@
+function calculateCartPrice(...num1){ // (...)it is also called rest  operator and spread operator .
+    return num1
+
+
+}
+//console.log(calculateCartPrice(200, 400,600));
+
+const user={
+    username:"ankit",
+    price:199
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleObject({
+    username:"sam",
+    price:399
+})
