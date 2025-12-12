c9489dbc74ce9132694243aa085e165357bad7c9
The1Ankit <ankitankush166@gmail.com>
2025-12-12 20:49:00 +0000

    save

---
+++ 02_switch.js
@@ -0,0 +1,26 @@
+//===========SWITCH CASE================
+
+const month=3
+switch (month) {
+    case 1:
+        console.log("january");
+        
+        break;
+    case 2:
+        console.log("febuary");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+
+    default:
+        console.log("Choose A wrong Input");
+        
+        break;
+}
--- 04_control_flow/01_one.js
+++ 04_control_flow/01_one.js
@@ -1,13 +1,26 @@
 //============if Statement====================
 
-const isUserLoggedin=true
+//const isUserLoggedin=true
 const temp=41
 if(temp<50){
 // console.log(3==="3");
 // console.log(3===3);
 // console.log(3==="a");
-console.log(`"temp is less then 50"${temp}`);
+// console.log(`"temp is less then 50"${temp}`);
 
 }
 
 //<(less than) ,>(greater then),<=(greater then or equal then),>=(less then or equal then ) ,==(both are equal 2==2),!=(not equal 3!=2 true),===(equal and data types also check 3==="3"(false),3===3(true))
+
+const userLoggedin=true
+const debitcard=true
+const loggedInFromGoogle=false
+const loggedInFromEmail=true
+if(userLoggedin && debitcard){
+    console.log("allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle){
+    console.log("allow to loggin");
+    
+}
