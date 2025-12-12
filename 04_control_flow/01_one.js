8493d296dc073381e6f4b1f58495a6856c90e018
The1Ankit <ankitankush166@gmail.com>
2025-12-12 16:07:58 +0000

    Save

---
+++ 04_control_flow/01_one.js
@@ -0,0 +1,13 @@
+//============if Statement====================
+
+const isUserLoggedin=true
+const temp=41
+if(temp<50){
+// console.log(3==="3");
+// console.log(3===3);
+// console.log(3==="a");
+console.log(`"temp is less then 50"${temp}`);
+
+}
+
+//<(less than) ,>(greater then),<=(greater then or equal then),>=(less then or equal then ) ,==(both are equal 2==2),!=(not equal 3!=2 true),===(equal and data types also check 3==="3"(false),3===3(true))
